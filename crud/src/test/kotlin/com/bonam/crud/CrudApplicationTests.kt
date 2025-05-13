a48a124261adada8539bc07b3c2dc8429c04283f
B0nam <daniiel.bonam@gmail.com>
2025-05-13 19:45:15 -0300

    chore: add project structure

---
+++ crud/src/test/kotlin/com/bonam/crud/CrudApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bonam.crud
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
